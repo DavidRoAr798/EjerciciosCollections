c463033b9915340ff2fc270e5cc19a484356e81f
David <david.rodr@gmail.com>
2016-07-08 09:31:42 +0200

    Ejercicio NumerosRandom

--- src/NumerosRandom.java
+++ src/NumerosRandom.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Main {
+public class NumerosRandom {
 
     public static void main(String[] args) {
         List<Integer> lista =new ArrayList<>();
